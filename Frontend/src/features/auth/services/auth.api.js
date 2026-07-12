08865f3ce9dc77d25639b8657aadd65da23f4ad5
Vishal Singh <vishalsingh23645@gmail.com>
2026-07-12 08:23:29 +0530

    Update frontend API URL

--- Frontend/src/features/auth/services/auth.api.js
+++ Frontend/src/features/auth/services/auth.api.js
@@ -2,7 +2,7 @@ import axios from "axios"
 
 
 const api = axios.create({
-    baseURL: "http://localhost:3000",
+    baseURL: "https://ai-interview-backend-av5f.onrender.com",
     withCredentials: true
 })
 
